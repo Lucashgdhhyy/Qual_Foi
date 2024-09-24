0d256bc95ce792e4247a56dd422a3c3ec6bed971
Lucashgdhhyy <155470121+Lucashgdhhyy@users.noreply.github.com>
2024-09-24 18:50:07 -0400

    Update Do_Bagulho.lua

--- Do_Bagulho.lua
+++ Do_Bagulho.lua
@@ -1,22 +1,12 @@
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
+local gameId = game.PlaceId
 
--- Função para detectar se o jogo é Blox Fruits
-local function isBloxFruits()
-    return game.PlaceId == 2753915549
-end
-
--- Função para detectar se o jogo é Prison Life
-local function isPrisonLife()
-    return game.PlaceId == 155615604
-end
-
--- Executar o script apropriado com base no jogo
-if isBloxFruits() then
+if gameId == 2753915549 then
+    -- Executa o script para Blox Fruits
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucashgdhhyy/Qual_Foi/refs/heads/main/blox.lua"))()
-elseif isPrisonLife() then
+elseif gameId == 155615604 then
+    -- Executa o script para Prison Life
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucashgdhhyy/Qual_Foi/refs/heads/main/prision.lua"))()
 else
-    -- Executar script de suporte se não estiver em um jogo suportado
+    -- Executa o script de suporte para outros jogos
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucashgdhhyy/Qual_Foi/refs/heads/main/support.lua"))()
 end
